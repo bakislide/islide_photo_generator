65490b6bb0467b948fc3ffedb4b9d29a1c0dace0
bakislide <noah.bakken@islideusa.com>
2024-07-16 09:45:53 -0400

    Done

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask
-from blueprints.main.routes import main
 from blueprints.image_processing.image_processing import image_processing
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
-app.register_blueprint(main)
-app.register_blueprint(image_processing)
+app.register_blueprint(image_processing, url_prefix='/image_processing')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
